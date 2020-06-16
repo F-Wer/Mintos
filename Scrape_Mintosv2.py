aaa049c69af2da529647a51b1a04a89d6b4befb9
F-Wer <43503689+F-Wer@users.noreply.github.com>
2020-06-16 13:52:19 +0200

    fixed reading file bug

--- Scrape_Mintosv2.py
+++ Scrape_Mintosv2.py
@@ -2,8 +2,10 @@ import codecs
 import smtplib
 import ssl
 import time
+from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from os.path import basename
 
 from selenium import webdriver
 
@@ -19,7 +21,7 @@ class ScrapeMintos(object):
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
         options.add_experimental_option('useAutomationExtension', False)
         print('Opening Chrome')
-        driver = webdriver.Chrome(options=options)
+        driver = webdriver.Chrome(options=options,executable_path=r'D:\Python\webdriver\chromedriver.exe')
         driver.execute_cdp_cmd("Network.setExtraHTTPHeaders", {"headers": {"User-Agent": "browserClientA"}})
         driver.get("https://www.mintos.com/de/login")
         inputElement_user = driver.find_element_by_id("login-username")
@@ -45,15 +47,25 @@ class ScrapeMintos(object):
             # a.close()
            # os.system(s_loc)
 
-    def sendmail(self):
+    def smail(self):
         print('Function Send_Mail')
         port = 465  # For SSL
         # Create a secure SSL context
         context = ssl.create_default_context()
         # reading the file in binary mode. Because it is saved as a UTF-8 file and there is a error, if you try to convert it to ASCII
-        r = open('D:/Python/webdriver/Mint.txt', "rb+")
+        r = open('D:/Python/webdriver/Mint.txt',encoding='utf-8')
+        files = ['D:/Python/webdriver/Mint.txt']
         msg = MIMEMultipart()
-        msg.attach(MIMEText(r.read()))
+        message = MIMEText(r.read())
+        msg.attach(message)
+        for f in files:
+            with open(f, "rb") as file:
+                part = MIMEApplication(
+                    file.read(),
+                    Name=basename(f)
+                )
+        part['Content-Disposition'] = 'attachment; filename="%s"' % basename(f)
+        msg.attach(part)
         msg['Subject'] = 'Mintos'
         msg['From'] = sender_email
         msg['To'] = receiver_email
@@ -66,5 +78,4 @@ class ScrapeMintos(object):
 
 
 o = ScrapeMintos()
-o.scrape()
-o.sendmail()
+o.smail()
